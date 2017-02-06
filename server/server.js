30e5cabc3fdbbc5f1a664db48b88dce2308cf08b
jww52 <jawszone@gmail.com>
2017-02-05 18:09:21 -0700

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,30 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey.  What is going on',
-  //   createAt: 123
-  // });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  //REMOVED AFTER io.emit created
-  // socket.emit('newMessage', {
-  //   from: 'john@example.com',
-  //   text: 'Hey.  this is new message event',
-  //   createAt: 1234
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail)
-  // });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message) => {
-    console.log('createMessage', message)
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', (socket) => {
@@ -47,4 +31,4 @@ io.on('connection', (socket) => {
 
 server.listen(port, () => {
   console.log(`started on port ${port}`);
-});
+  });
